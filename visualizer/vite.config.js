cb07a9378dc80ef531f0b473f7ac015051d90585
Mike Rotondo <mrotondo@gmail.com>
2023-11-22 12:48:16 -0800

    Add deploy workflow

--- visualizer/vite.config.js
+++ visualizer/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/unroads/',
   server: {
     hmr:
       process.env.CODESANDBOX_SSE || process.env.GITPOD_WORKSPACE_ID
